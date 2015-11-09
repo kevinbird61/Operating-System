785d6d137ee8c533da2b52578727f7eb7598d935
kevinbird61 <kevinbird61@gmail.com>
2015-11-10 00:05:17 +0800

    Upload for new project starting - "Multiprocess"
    FirstFork - let me know the fork() operating progress.
    TestPid - Test for the fork() for multi-processor.
    Hw3 - For the project goal.

---
+++ Multiprocess/TestPid/ControlForkNum.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc , char *argv[]){
+	int i,control;
+	pid_t pid;
+	control = atoi(argv[1]);
+	printf("input comm is %d\n\n",control);
+	if(control != 1){
+		/* Create the need amount of threads */
+		pid = fork();
+		for(i=1;i<control-1;i++)
+		{
+			if(pid==0){
+				pid = fork();
+			}
+		}
+		/* Do the threads jobs */
+		if(pid>0){
+			printf("This is iterative parent pid : %d\n",pid);
+		}
+		else{
+			printf("This is iterative child pid %d\n",pid);
+		}
+	}
+	else{
+		printf("Only one Thread!\n");
+		/*TODO do one thread things*/
+	}
+	wait(NULL);
+	printf("Pid %d end\n",pid);
+	return 0;
+}
